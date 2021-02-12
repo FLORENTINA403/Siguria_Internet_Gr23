22fd8f1c76126245c26a44a30aaf983799aa08ed
ErzaKrasniqi <ereza.krasniqi2@gmail.com>
2021-02-12 18:40:52 +0100

    Xss vulnerability analysis per forma

--- siguri/siguri/siguri.py
+++ siguri/siguri/siguri.py
@@ -22,6 +22,46 @@ def get_form_details(form):
     details["method"] = method
     details["inputs"] = inputs
     return details
+def submit_form(form_details, url, value):
+    target_url = urljoin(url, form_details["action"])
+    inputs = form_details["inputs"]
+    data = {}
+    for input in inputs:
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            data[input_name] = input_value
+
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        return requests.get(target_url, params=data)
+
+
+def scan_xss(url):
+
+    forms = get_all_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+    js_script = "<Script>alert('hi')</scripT>"
+    is_vulnerable = False
+    for form in forms:
+        form_details = get_form_details(form)
+        content = submit_form(form_details, url, js_script).content.decode()
+        if js_script in content:
+            print(f"[+] XSS Detected on {url}")
+            print(f"[*] Form details:")
+            pprint(form_details)
+            is_vulnerable = True
+    return is_vulnerable
+
+if __name__ == "__main__":
+    url = "http://testphp.vulnweb.com/login.php"
+    print(scan_xss(url))
+
+
+
 
 
 
