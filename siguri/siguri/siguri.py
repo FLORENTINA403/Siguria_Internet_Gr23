4a138073c15c194e98d71d90f4a67d7d172580e6
florifrrokaj <florifrrokaj00@gmail.com>
2021-02-13 17:40:07 +0100

    Error handling

--- siguri/siguri/siguri.py
+++ siguri/siguri/siguri.py
@@ -7,62 +7,81 @@ from tkinter.ttk import Combobox
 import tkinter.scrolledtext as st
 
 def get_all_forms(url):
-    soup = bs(requests.get(url).content, "html.parser")
+    try:
+        soup = bs(requests.get(url).content, "html.parser")
+        
+    except Exception as e:
+        error = e.read()
+        output.insert(tk.INSERT, error)
     return soup.find_all("form")
 
 
 def get_form_details(form):
-   
-    details = {}
-    action = form.attrs.get("action").lower()
-    method = form.attrs.get("method", "get").lower()
-    inputs = []
-    for input_tag in form.find_all("input"):
-        input_type = input_tag.attrs.get("type", "text")
-        input_name = input_tag.attrs.get("name")
-        inputs.append({"type": input_type, "name": input_name})
-    details["action"] = action
-    details["method"] = method
-    details["inputs"] = inputs
-    return details
+   try:
+       details = {}
+       action = form.attrs.get("action").lower()
+       method = form.attrs.get("method", "get").lower()
+       inputs = []
+       for input_tag in form.find_all("input"):
+           input_type = input_tag.attrs.get("type", "text")
+           input_name = input_tag.attrs.get("name")
+           inputs.append({"type": input_type, "name": input_name})
+       details["action"] = action
+       details["method"] = method
+       details["inputs"] = inputs
+       
+   except Exception as e:
+       error = e.read()
+       output.insert(tk.INSERT, error)
+   return details
 def submit_form(form_details, url, value):
-    target_url = urljoin(url, form_details["action"])
-    inputs = form_details["inputs"]
-    data = {}
-    for input in inputs:
-        if input["type"] == "text" or input["type"] == "search":
-            input["value"] = value
-        input_name = input.get("name")
-        input_value = input.get("value")
-        if input_name and input_value:
-            data[input_name] = input_value
-
-    if form_details["method"] == "post":
-        return requests.post(target_url, data=data)
-    else:
-        return requests.get(target_url, params=data)
-
+    try:
+        target_url = urljoin(url, form_details["action"])
+        inputs = form_details["inputs"]
+        data = {}
+        for input in inputs:
+            if input["type"] == "text" or input["type"] == "search":
+                input["value"] = value
+            input_name = input.get("name")
+            input_value = input.get("value")
+            if input_name and input_value:
+                data[input_name] = input_value
+     except Exception as e:
+        error = e.read()
+        output.insert(tk.INSERT, error)
+     if form_details["method"] == "post":
+         return requests.post(target_url, data=data)
+     else:
+         return requests.get(target_url, params=data)
+   
 
 def scan_xss(url):
+    try:
+        # get all the forms from the URL
+        forms = get_all_forms(url)
+        step_1 = str(f"[+] Detected {len(forms)} forms on {url}.")
+        js_script = "<Script>alert('hi')</scripT>"
+        # returning value
+        is_vulnerable = False
+        # iterate over all forms
 
-    forms = get_all_forms(url)
-    print(f"[+] Detected {len(forms)} forms on {url}.")
-    js_script = "<Script>alert('hi')</scripT>"
-    is_vulnerable = False
-    for form in forms:
-        form_details = get_form_details(form)
-        content = submit_form(form_details, url, js_script).content.decode()
-        if js_script in content:
-            print(f"[+] XSS Detected on {url}")
-            print(f"[*] Form details:")
-            pprint(form_details)
-            is_vulnerable = True
-    return is_vulnerable
-
-if __name__ == "__main__":
-    url = "http://testphp.vulnweb.com/login.php"
-    print(scan_xss(url))
-
+        for form in forms:
+            form_details = get_form_details(form)
+            content = submit_form(form_details, url, js_script).content.decode()
+            if js_script in content:
+                step_2 = str(f"[+] XSS Detected on {url}")
+                step_3 = str(f"[*] Form details:")
+            
+                is_vulnerable = True
+            # won't break because we want to print available vulnerable forms
+    except Exception as e:
+        error = e.read()
+        return error        
+    if True:        
+         return step_1 +"\n" + step_2 + "\n" + step_3 + "\n"+str(form_details)
+    else:
+         return "something went worng"
+    
 #window
 window = tk.Tk()
 window.title("XSS Testing")
@@ -90,7 +109,13 @@ url.configure(selectforeground="black")
 
 def clicked():
    res = url.get()
-   #label.configure(text=res)
+   if res.startswith("http://") != True and res.startswith("https://") != True:
+      result = "http://" + res
+      txt = scan_xss(result)
+   else:
+      txt = scan_xss(res)
+
+   output.insert(tk.INSERT, txt)
 
 #BruteXSS button
 button = tk.Button(window, width=15,activebackground='#FFFFFF',command=clicked)
